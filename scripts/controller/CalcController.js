675ccb330bcec01933b9ce35fc99ac0f79167be5
Débora D'amato <debora_damato@hotmail.com>
2021-10-20 19:56:59 -0300

    Atualizando Display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -63,36 +63,73 @@ class CalcController {
 
     }
 
+    pushOperation(value) {
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
+
+    }
+
+    calc(){
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+    }
+
+    setLastNumberToDisplay(){
+
+        
+
+    }
+
     addOperation(value){
 
-        console.log('A', isNaN(this.getLastOperation()));
+        console.log(isNaN(this.getLastOperation()));
 
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
                
-                this._setLastOperation(value);
+                this.setLastOperation(value);
                 
             } else if(isNaN(value)) {
 
                 //Outra coisa
-                console.log(value);
+               //Outra coisa console.log(value);
 
 
             } else {
 
-                this._operation.push(value);
+                this.pushOperation(value);
 
             } 
 
         } else {
 
-           let newValue = this.getLastOperation().toString() + value.toString();
-           this.setLastOperation(parseInt(newValue));
+            if (this.isOperator(value)) {
+
+                this.pushOperation(value);
+
+            } else {
+                
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+
+            }
 
         }
 
-        console.log(this._operation);
 
     }
 
